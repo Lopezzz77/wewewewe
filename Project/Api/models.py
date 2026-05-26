9044a1384bce75c9b3e8fb6c1a3f2847054a8352
Cesariño <cesargabrielolopez77@gmail.com>
2026-05-26 10:39:28 -0300

    DB

--- Project/Api/models.py
+++ Project/Api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# clss+name 
+
+class Alumno(models.Model):
+    id_Alumno = models.AutoField(primary_key=True)
+    DNI = models.TextField(max_length=10, unique=True)
+    Edad = models.IntegerField()
+    nombre = models.TextField(max_length=35)
+    apellido = models.TextField(max_length=35)
+    email = models.EmailField(unique=True)
+    Calle = models.TextField(max_length=50)
+    Altura= models.IntegerField()
+
+    def __str__(self):
+        return self.DNI 
